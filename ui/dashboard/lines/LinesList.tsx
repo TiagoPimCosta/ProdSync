cde4e1dbde22e1d25b56563ca111e05b48f44c07
TiagoPimCosta <tiagopimcosta@gmail.com>
2025-04-08 20:06:22 +0100

    feat: Add Pagination and Page Size Selection to Lines and Machines Lists
    - Integrated pagination functionality in LinesList and MachinesList components.
    - Added page size selection using a dropdown for better user experience.
    - Updated routing to reflect changes in page and size parameters.
    - Enhanced layout to display total items and current page information.

--- ui/dashboard/lines/LinesList.tsx
+++ ui/dashboard/lines/LinesList.tsx
@@ -2,10 +2,15 @@
 
 import { useGetLines } from "@/src/services/lines/queries";
 import LineCard from "./LineCard";
-import { useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
+import { PageSizes } from "@/src/utils/consts";
+import { Pagination } from "@mantine/core";
+import Select from "@/src/components/ui/Select";
 
 export default function LinesList() {
   const linesSearchParams = useSearchParams();
+  const router = useRouter();
+  const { push } = router;
 
   const page = Number(linesSearchParams.get("page")) || 1;
   const size = Number(linesSearchParams.get("size")) || 10;
@@ -19,11 +24,53 @@ export default function LinesList() {
     status: status,
   });
 
+  const handleChangePage = (page: number) => {
+    const currentParams = new URLSearchParams(linesSearchParams.toString());
+    currentParams.set("page", page.toString());
+    push(`?${currentParams.toString()}`);
+  };
+
+  const handleChangePageSize = (pageSize: string | undefined) => {
+    if (pageSize) {
+      const currentParams = new URLSearchParams(linesSearchParams.toString());
+      currentParams.set("size", pageSize);
+      currentParams.set("page", "0");
+      push(`?${currentParams.toString()}`);
+    }
+  };
+
+  if (!data?.items?.length) return <div className=" pt-5 h-24 text-center">No results</div>;
+
   return (
-    <div className="grid gap-6 md:grid-cols-2">
-      {data?.items?.map((line) => (
-        <LineCard key={`line-${line.id}`} line={line} />
-      ))}
-    </div>
+    <>
+      <div className="grid gap-6 md:grid-cols-2">
+        {data.items.map((line) => (
+          <LineCard key={`line-${line.id}`} line={line} />
+        ))}
+      </div>
+      <div className="w-full">
+        <div className="flex justify-between items-center pl-4 pr-2 py-2">
+          <div className="flex-1 justify-start">
+            {(page - 1) * size + 1}-{Math.min(page * size, data?.totalItems || 0)} of{" "}
+            {data?.totalItems || 0} items
+          </div>
+          <div className="flex-1 flex justify-center">
+            <Pagination
+              total={Math.ceil((data?.totalItems || 0) / (size || 1))}
+              value={page}
+              onChange={handleChangePage}
+            />
+          </div>
+          <div className="flex-1 flex justify-end">
+            <Select
+              className="w-20"
+              data={PageSizes}
+              value={size.toString()}
+              onChange={handleChangePageSize}
+            />
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
--- ui/dashboard/machines/MachinesList.tsx
+++ ui/dashboard/machines/MachinesList.tsx
@@ -1,11 +1,16 @@
 "use client";
 
-import { useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import { useGetMachines } from "@/src/services/machines/queries";
 import MachineCard from "./MachineCard";
+import Select from "@/src/components/ui/Select";
+import { Pagination } from "@mantine/core";
+import { PageSizes } from "@/src/utils/consts";
 
 export default function MachinesList() {
   const machinesSearchParams = useSearchParams();
+  const router = useRouter();
+  const { push } = router;
 
   const page = Number(machinesSearchParams.get("page")) || 1;
   const size = Number(machinesSearchParams.get("size")) || 10;
@@ -23,13 +28,53 @@ export default function MachinesList() {
     status,
   });
 
+  const handleChangePage = (page: number) => {
+    const currentParams = new URLSearchParams(machinesSearchParams.toString());
+    currentParams.set("page", page.toString());
+    push(`?${currentParams.toString()}`);
+  };
+
+  const handleChangePageSize = (pageSize: string | undefined) => {
+    if (pageSize) {
+      const currentParams = new URLSearchParams(machinesSearchParams.toString());
+      currentParams.set("size", pageSize);
+      currentParams.set("page", "0");
+      push(`?${currentParams.toString()}`);
+    }
+  };
+
   if (!data?.items?.length) return <div className=" pt-5 h-24 text-center">No results</div>;
 
   return (
-    <div className="grid gap-4 md:grid-cols-4">
-      {data?.items?.map((machine) => (
-        <MachineCard key={`machine-${machine.id}`} machine={machine} />
-      ))}
-    </div>
+    <>
+      <div className="grid gap-4 md:grid-cols-4">
+        {data?.items?.map((machine) => (
+          <MachineCard key={`machine-${machine.id}`} machine={machine} />
+        ))}
+      </div>
+      <div className="w-full">
+        <div className="flex justify-between items-center pl-4 pr-2 py-2">
+          <div className="flex-1 justify-start">
+            {(page - 1) * size + 1}-{Math.min(page * size, data?.totalItems || 0)} of{" "}
+            {data?.totalItems || 0} items
+          </div>
+          <div className="flex-1 flex justify-center">
+            <Pagination
+              total={Math.ceil((data?.totalItems || 0) / (size || 1))}
+              value={page}
+              onChange={handleChangePage}
+            />
+          </div>
+          <div className="flex-1 flex justify-end">
+            <Select
+              className="w-20"
+              data={PageSizes}
+              value={size.toString()}
+              onChange={handleChangePageSize}
+            />
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
