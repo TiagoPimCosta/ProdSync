276e48eab56111c97333282cc5bca6aac130f91b
TiagoPimCosta <tiagopimcosta@gmail.com>
2026-03-29 23:41:00 +0100

    refactor: standardize string quotes and clean up RecordsTable component

--- client/ui/dashboard/records/RecordsTable.tsx
+++ client/ui/dashboard/records/RecordsTable.tsx
@@ -1,40 +1,35 @@
-"use client";
+'use client';
 
-import { useRouter, useSearchParams } from "next/navigation";
-import { ColumnDef, createColumnHelper } from "@tanstack/react-table";
-import { DataTable } from "@/src/components/ui/data-table";
-import { RecordObj, useGetRecords } from "@/src/services/records/queries";
-import dayjs from "dayjs";
+import { useRouter, useSearchParams } from 'next/navigation';
+import { ColumnDef, createColumnHelper } from '@tanstack/react-table';
+import { DataTable } from '@/src/components/ui/data-table';
+import { RecordObj, useGetRecords } from '@/src/services/records/queries';
+import dayjs from 'dayjs';
 
 export default function RecordsTable() {
   const recordsSearchParams = useSearchParams();
   const { push } = useRouter();
 
-  const page = Number(recordsSearchParams.get("page")) || 1;
-  const size = Number(recordsSearchParams.get("size")) || 10;
-  const user = recordsSearchParams.get("user") || undefined;
-  const machine = recordsSearchParams.get("machine") || undefined;
-  const startPeriod = recordsSearchParams.get("startPeriod") || undefined;
-  const endPeriod = recordsSearchParams.get("endPeriod") || undefined;
+  const page = Number(recordsSearchParams.get('page')) || 1;
+  const size = Number(recordsSearchParams.get('size')) || 10;
+  const user = recordsSearchParams.get('user') || undefined;
+  const machine = recordsSearchParams.get('machine') || undefined;
+  const startPeriod = recordsSearchParams.get('startPeriod') || undefined;
+  const endPeriod = recordsSearchParams.get('endPeriod') || undefined;
 
   const columnHelper = createColumnHelper<RecordObj>();
   const tableColumns = [
-    columnHelper.accessor("id", {
-      header: "Numero",
-      size: 5,
-      minSize: 5,
+    columnHelper.accessor('user.name', {
+      header: 'Nome',
     }),
-    columnHelper.accessor("user.name", {
-      header: "Nome",
+    columnHelper.accessor('machine.name', {
+      header: 'Maquina',
     }),
-    columnHelper.accessor("machine.name", {
-      header: "Maquina",
-    }),
-    columnHelper.accessor("createdAt", {
-      header: "Registo",
+    columnHelper.accessor('createdAt', {
+      header: 'Registo',
       cell: ({ row }) => {
         const createdAt = row.original.createdAt;
-        return dayjs(createdAt).format("YYYY/MM/DD HH:mm:ss");
+        return dayjs(createdAt).format('YYYY/MM/DD HH:mm:ss');
       },
     }),
   ] as ColumnDef<RecordObj>[];
@@ -50,15 +45,15 @@ export default function RecordsTable() {
 
   const handleChangePage = (page: number) => {
     const currentParams = new URLSearchParams(recordsSearchParams.toString());
-    currentParams.set("page", page.toString());
+    currentParams.set('page', page.toString());
     push(`?${currentParams.toString()}`);
   };
 
   const handleChangePageSize = (pageSize: string | undefined) => {
     if (pageSize) {
       const currentParams = new URLSearchParams(recordsSearchParams.toString());
-      currentParams.set("size", pageSize);
-      currentParams.set("page", "0");
+      currentParams.set('size', pageSize);
+      currentParams.set('page', '0');
       push(`?${currentParams.toString()}`);
     }
   };
